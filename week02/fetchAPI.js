134b0179a059e2ff897f58377fd576f02cbaa3bf
jonalldr <jonahalldredge1@gmail.com>
2026-09-13 18:32:43 -0600

    started directory assignment

---
+++ week02/fetchAPI.js
@@ -0,0 +1,9 @@
+
+async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/'); // request
+    const data = await response.json(); //parse the JSON data
+    console.log(data); // temp output test of data response
+    // console.log(await response.json()); This gives an error
+}
+
+getData();
